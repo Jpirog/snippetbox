371e58d6d2d71f4ecd764e8dbc1f5b03c7c5377a
John Pirog <johnpirog@Johns-MacBook-Pro.local>
2022-10-09 22:30:07 -0500

    4.4

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -20,6 +23,14 @@ func main() {
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime|log.Llongfile)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
+
+	dsn := flag.String("dsn", "root:mysql@tcp(localhost:3306)/snippetbox?parseTime=true", "MySQL data source name")
+	infoLog.Printf("Connecting to DB: %v", *dsn)
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -36,4 +47,17 @@ func main() {
 	infoLog.Printf("Starting server on port %v ", fullname)
 	herr := srv.ListenAndServe()
 	errorLog.Fatal(herr)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
